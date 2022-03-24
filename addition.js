a56adf93d2bc3192340dc36c5f5895952757833c
Dhruvi2908 <102288904+Dhruvi2908@users.noreply.github.com>
2022-03-24 14:57:42 +0530

    1
    1

---
+++ addition.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+var c=a+b;
+var s=a-b;
+var m=a*b;
+var d=a/b;
+console.log("Value of a:"+a);
+console.log("Value of b:"+b);
+console.log("Addition is:"+c);
+console.log("Subtraction is:"+s);
+console.log("Multiplication is:"+m);
+console.log("Division is:"+d);
