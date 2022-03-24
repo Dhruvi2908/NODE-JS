1604fa5a133777b1102c32af7a083149cc50d48b
Dhruvi2908 <102288904+Dhruvi2908@users.noreply.github.com>
2022-03-24 15:10:38 +0530

    Add files via upload

---
+++ serverdemo2.js
@@ -0,0 +1,10 @@
+var http=require('http');
+var a=10;
+var b=20;
+var c=a+b;
+var msg = `A Value is ${a} <br/> B Value is ${b} and  <br/>Sum is ${c}`;
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'})
+    res.end(msg);
+}).listen(3000);
+console.log("Server is started http://127.0.0.1:3000");
